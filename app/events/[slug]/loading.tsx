cd59e9c8d64ed263e9c4e164dac99c3779775d6c
Louie Liu <Louie.p.Liu@gmail.com>
2025-12-21 20:32:56 +0800

    finalize the app

---
+++ app/events/[slug]/loading.tsx
@@ -0,0 +1,8 @@
+// app/events/[slug]/loading.tsx
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p>正在加载活动详情...</p>
+    </div>
+  );
+}
